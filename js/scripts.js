363d7921d0772881ee3df6cf6166d95d41d50237
Mubashir Saeed <mubashirsaeed234@gmail.com>
2024-08-05 12:49:06 +0500

    add skills section using swiper

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,38 @@
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    grapCursor: true,
+    centeredSlides: true,
+    spaceBetween: true,
+
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dymanicBllets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        769: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+          },
+         1024: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+        }
+  });
